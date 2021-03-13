97d764e9b67d61b03ab7a0659aa1f79187b72529
markkiths <markkiths@gmail.com>
2021-03-13 15:23:16 +0700

    msm8916-common: Add libloc_ds_api from crackling TOS111B https://github.com/lettuce-oreo/OLD-vendor_yu_lettuce/commit/9fe52be9c5adf435d9ad38965a88bc097fb76f89

--- msm8916-common/Android.mk
+++ msm8916-common/Android.mk
@@ -31,6 +31,17 @@ LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libloc_ds_api
+LOCAL_MODULE_OWNER := asus
+LOCAL_SRC_FILES_64 := proprietary/lib64/libloc_ds_api.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libloc_ds_api.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libmm-abl
 LOCAL_MODULE_OWNER := asus
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libmm-abl.so
